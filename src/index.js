cb1f6a998e026bf170be0130e0499ffe5c677795
Douglas Brandão <38235074+douglasbrandao21@users.noreply.github.com>
2019-09-08 14:59:44 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { , Component } from "react";
+import React, { Fragment, Component } from "react";
 import { StatusBar } from "react-native";
 import CodePush from "react-native-code-push";
-import OneSignal from 'react-native-onesignal'
+import OneSignal from "react-native-onesignal";
 
 import "./config/ReactotronConfig";
 
@@ -11,21 +11,26 @@ import store from "./store";
 import Routes from "~/routes";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     OneSignal.init("6417cb47-1e8f-4ad4-ae37-80d918819bcb");
     OneSignal.addEventListener("received", this.onReceived);
-    OneSignal.addEventListener("opened", this.onOpened)
-    OneSignal.addEventListener("ids", this.onIds)
+    OneSignal.addEventListener("opened", this.onOpened);
+    OneSignal.addEventListener("ids", this.onIds);
   }
 
   componentWillUnmount() {
     OneSignal.init("6417cb47-1e8f-4ad4-ae37-80d918819bcb");
     OneSignal.removeEventListener("received", this.onReceived);
-    OneSignal.removeEventListener("opened", this.onOpened)
-    OneSignal.removeEventListener("ids", this.onIds)
+    OneSignal.removeEventListener("opened", this.onOpened);
+    OneSignal.removeEventListener("ids", this.onIds);
   }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
 
   render() {
     return (
@@ -33,7 +38,7 @@ class App extends Component {
         <Routes />
         <StatusBar backgroundColor="#0C0A17" barStyle="light-content" />
       </Fragment>
-    )
+    );
   }
 }
 export default CodePush({
